7f837c97ffcc0a30353267becbfbf8b8464dc1dd
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 16:57:25 -0300

    Add files via upload

---
+++ Desafio46.py
@@ -0,0 +1,8 @@
+# Contagem regressiva
+import emoji
+from time import sleep
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('Happy New Year!!!')
+print(emoji.emojize(':boom:' * 8, language='alias'))
